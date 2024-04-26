26418988fced0a700bdce022d3525d2e4ad44680
nafin-nm1913152 <nafinbd24@gmail.com>
2024-04-26 15:42:00 +0300

    up

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from transformers import BertTokenizer, TFBertForSequenceClassification
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"text": "This hate speech model api point"}
+
 def get_model():
     tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
     model = TFBertForSequenceClassification.from_pretrained("Nafin/hate_speech")
@@ -55,84 +59,3 @@ async def read_root(request: Request):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
-
-
-# ---------------------------------------------------------------------------------------
-
-
-# from fastapi import FastAPI
-# from pydantic import BaseModel
-# from transformers import BertTokenizer, BertForSequenceClassification
-# import torch
-
-# app = FastAPI()
-
-# class InputText(BaseModel):
-#     text: str
-
-# # Load your model
-# model = BertForSequenceClassification.from_pretrained("Nafin/hate_speech_bert_extension")
-# tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-
-# @app.post("/predict/")
-# def predict(input_text: InputText):
-#     # Tokenize input text
-    
-#     encoded_input = tokenizer(input_text.text, return_tensors="pt", padding=True, truncation=True)
-    
-#     # Run model prediction
-#     with torch.no_grad():
-#         output = model(**encoded_input)
-#         logits = output.logits
-    
-#     # Get predicted class
-#     predicted_class = torch.argmax(logits, dim=1).item()
-    
-#     # Define class labels
-#     class_labels = {0: 'Hate Speech', 1: 'Offensive Language', 2: 'Neither'}
-    
-#     # Return predicted class
-#     return {"prediction": class_labels[predicted_class]}
-
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-
-# ---------------------------------------------------------
-
-# from fastapi import FastAPI
-# from pydantic import BaseModel
-# from transformers import BertTokenizer, TFBertForSequenceClassification
-# import tensorflow as tf
-
-# app = FastAPI()
-
-# class InputText(BaseModel):
-#     text: str
-
-# # Load your model
-# model = TFBertForSequenceClassification.from_pretrained("Nafin/hate_speech_bert_extension")
-# tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-
-# @app.post("/predict/")
-# def predict(input_text: InputText):
-#     # Tokenize input text
-#     inputs = tokenizer(input_text.text, return_tensors="tf", padding=True, truncation=True)
-
-#     # Run model prediction
-#     output = model(inputs)
-
-#     # Get predicted class
-#     predicted_class = tf.argmax(output.logits, axis=1).numpy()[0]
-
-#     # Define class labels
-#     class_labels = {0: 'Hate Speech', 1: 'Offensive Language', 2: 'Neither'}
-
-#     # Return predicted class
-#     return {"prediction": class_labels[predicted_class]}
-
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
